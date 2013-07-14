4f0bc870f77cc4ac679fdc040f9ffbf7ebf9e3b1
Gabriel Schade Cardoso <gabriel.schade@hotmail.com>
2013-07-14 00:25:29 -0300

    Update

---
+++ Capitulo7/AuxiliarKinect - Versão 3/AuxiliarKinect/Movimentos/Rastreador.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuxiliarKinect.Movimentos
+{
+    public class Rastreador<T> : IRastreador where T : Movimento, new()
+    {
+        private T movimento;
+        public EstadoRastreamento EstadoAtual { get; private set; }
+        public event EventHandler MovimentoIdentificado;
+
+        public Rastreador()
+        {
+            EstadoAtual = EstadoRastreamento.NaoIdentificado;
+            movimento = Activator.CreateInstance<T>();
+        }
+
+        public void Rastrear(Skeleton esqueletoUsuario)
+        {
+            EstadoRastreamento novoEstado = movimento.Rastrear(esqueletoUsuario);
+
+            if (novoEstado == EstadoRastreamento.Identificado &&
+                EstadoAtual != EstadoRastreamento.Identificado)
+                ChamarEvento(MovimentoIdentificado);
+
+            EstadoAtual = novoEstado;
+        }
+
+        private void ChamarEvento(EventHandler evento)
+        {
+            if (evento != null)
+                evento(movimento, new EventArgs());
+        }
+
+    }
+}
---
+++ Capitulo7/AuxiliarKinect - Versão 4/AuxiliarKinect/Movimentos/Rastreador.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuxiliarKinect.Movimentos
+{
+    public class Rastreador<T> : IRastreador where T : Movimento, new()
+    {
+        private T movimento;
+        public EstadoRastreamento EstadoAtual { get; private set; }
+        public event EventHandler MovimentoIdentificado;
+        public event EventHandler MovimentoEmProgresso;
+
+        public Rastreador()
+        {
+            EstadoAtual = EstadoRastreamento.NaoIdentificado;
+            movimento = Activator.CreateInstance<T>();
+        }
+
+        public void Rastrear(Skeleton esqueletoUsuario)
+        {
+            EstadoRastreamento novoEstado = movimento.Rastrear(esqueletoUsuario);
+
+            if (novoEstado == EstadoRastreamento.Identificado &&
+                EstadoAtual != EstadoRastreamento.Identificado)
+                ChamarEvento(MovimentoIdentificado);
+
+            if (novoEstado == EstadoRastreamento.EmExecucao &&
+                (EstadoAtual == EstadoRastreamento.EmExecucao ||
+                  EstadoAtual == EstadoRastreamento.NaoIdentificado))
+                ChamarEvento(MovimentoEmProgresso);
+
+            EstadoAtual = novoEstado;
+        }
+
+        private void ChamarEvento(EventHandler evento)
+        {
+            if (evento != null)
+                evento(movimento, new EventArgs());
+        }
+
+    }
+}
